f729712ce84ac77d8de655bd4b0e2c0af60e58a3
Your Name <you@example.com>
2020-11-17 11:22:52 +0800

    merge2

--- src/router/index.js
+++ src/router/index.js
@@ -5,32 +5,27 @@ import Index from '../views/user/Index.vue'
 Vue.use(VueRouter)
 
 const routes = [{
-            path: '/index',
-            name: 'index',
-            component: Index
-        },
-        {
-            path: '/index4',
-            name: 'index',
-            component: Index
-        }
-        const routes = [{
-                path: '/index',
-                name: 'index',
-                component: Index
-            },
-            {
-                path: '/index2',
-                name: 'index',
-                component: Index
-            }
-        ]
-        const router = new VueRouter({
-            // mode: 'history',
-            // base: process.env.BASE_URL,
-            // routes
-            routes,
-            mode: 'history'
-        })
+        path: '/index',
+        name: 'index',
+        component: Index
+    },
+    {
+        path: '/index4',
+        name: 'index',
+        component: Index
+    },
+    {
+        path: '/index5',
+        name: 'index',
+        component: Index
+    }
+]
+const router = new VueRouter({
+    // mode: 'history',
+    // base: process.env.BASE_URL,
+    // routes
+    routes,
+    mode: 'history'
+})
 
-        export default router
+export default router
