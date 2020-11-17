59cbe8c6a896e47c1d1fa6b510b4b1f42c197055
Your Name <you@example.com>
2020-11-17 11:03:06 +0800

    t1

--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,10 @@ import Index from '../views/user/Index.vue'
 Vue.use(VueRouter)
 
 const routes = [{
-        path: '/index',
-        name: 'index',
-        component: Index
-    },
-    {
-        path: '/index3',
-        name: 'index',
-        component: Index
-    }
-]
+    path: '/index',
+    name: 'index',
+    component: Index
+}]
 const router = new VueRouter({
     // mode: 'history',
     // base: process.env.BASE_URL,
